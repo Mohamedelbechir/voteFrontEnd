966a38b042db4dae8a2fc509971adec82dc2d05a
mohamedelbechir <mohamedelbechir1@gmail.com>
2019-10-26 20:18:35 +0200

    first commit front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlimComponent } from './slim/slim.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
+import { ContactComponent } from './contact/contact.component';
+import { GaleryComponent } from './galery/galery.component';
+import { IsieComponent } from './isie/isie.component';
+import { AboutComponent } from './about/about.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlimComponent,
+    HeaderComponent,
+    FooterComponent,
+    IntroComponent,
+    ContactComponent,
+    GaleryComponent,
+    IsieComponent,
+    AboutComponent,
+    NewsletterComponent
   ],
   imports: [
     BrowserModule,
